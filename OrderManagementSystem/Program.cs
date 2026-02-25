dfa3c8bed139e0046d9b1c76bd5bf5534c48d752
Satyarth Programming Hub <satyarthprogramminghub@gmail.com>
2026-02-25 22:38:58 +1100

    Add project files.

---
+++ OrderManagementSystem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OrderManagementSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
